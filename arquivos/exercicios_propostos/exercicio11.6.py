f0715baf60ca548e842dff6f9aae563f1fdaa6c9
Marcos Costa <marcoss.analytics@gmail.com>
2025-01-15 11:00:06 -0300

    Exercicio Proposto 11.6

--- arquivos/exercicios_propostos/exercicio11.6.py
+++ arquivos/exercicios_propostos/exercicio11.6.py
@@ -1,14 +1,33 @@
+'''Escreva um programa que leia um arquivo de entrada contendo números inteiros, sendo um por
+linha, e os coloque em uma lista. Em seguida pense em alguma forma de remover os valores
+repetidos, deixando apenas uma cópia de cada valor.
+A lista resultante após a eliminação dos repetidos, deve ser ordenada e salva no arquivo
+UNICOS.TXT, um inteiro por linha.'''
 
-entrada = int(input("Digite um valor inteiro: "))
+import random
 
+lista_numeros = [random.randint(1,2000) for _ in range(5000)]
+with open("numeros_aleatorios.txt", "w") as arquivo_lista:
+    for numero in lista_numeros:
+        arquivo_lista.write(f"{numero}\n")
+
+# Lê os números do arquivo e adiciona à lista
 lista = []
+with open("numeros_aleatorios.txt", "r") as arquivo_lista:
+    for num in arquivo_lista:
+       lista.append(int(num.strip()))   # Converte as linhas em inteiros
 
-arquivo = open('C:/Users/estev/ESTUDOS-PYTHON/arquivos/exercicios_propostos/UNICOS.TXT', 'w')
-for _ in entrada:    
-    arquivo.write(f"{entrada}")
+# Remove os duplicados e ordena a lista
+# set(lista): Converte a lista para um conjunto, removendo automaticamente os valores duplicados.
+# sorted(set(lista)) : Ordena o conjunto resultante, garantindo que os números fiquem em ordem crescente.
+lista_numeros = sorted(set(lista))
 
-lista.append(arquivo)
-set(lista)
-lista.sort()
+print(lista)
 
-arquivo.close()
+# Salva os números únicos e ordenados no arquivo "UNICOS.TXT"
+with open("UNICOS.TXT", "w") as arquivo_unicos:
+    for numero in lista_numeros:
+        arquivo_unicos.write(f"{numero}\n")
+
+
+
